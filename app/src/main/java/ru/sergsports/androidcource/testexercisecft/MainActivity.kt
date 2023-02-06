70789dcbba779d8b11851b240b9f982b0a10aa0f
Sergey Korchagin <kor.work@mail.ru>
2023-02-06 13:58:25 +0800

    Initial commit Предварительно сделана верстка экранов

---
+++ app/src/main/java/ru/sergsports/androidcource/testexercisecft/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.sergsports.androidcource.testexercisecft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
